8b15bd5b95f2eadc7bd6eb3754f0ff1be215eeeb
Dustin Doell <doelldd@gmail.com>
2020-11-19 23:37:10 -0700

    Create an array of settlement objects dynamically without duplicates

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -15,7 +15,18 @@ export default {
   mounted: function() {
 
 
-    const draw = SVG().addTo('#board').size('100%', '100%')
+    const draw = SVG().addTo('#board').size('100%', '100%');
+
+    // Draw the settlements
+    const renderSettlements = (settlement) => {
+      const { x, y } = settlement.point;
+      const r = 15;
+      draw
+        .circle(r * 2)
+        .stroke({ width: 5, color: '#000' })
+        .fill('none')
+        .translate(x - r, y - r)
+    };
 
     // Hex object
     const Hex = Honeycomb.extendHex({
@@ -32,16 +43,15 @@ export default {
           .translate(x, y)
       },
 
-      renderSettlements(draw) {
+      renderOcean(draw) {
         const { x, y } = this.toPoint()
-        const corners = this.corners();
-        const r = 10;
-        corners.forEach(corner => {
-          this.drawSettlements = draw
-            .circle(r * 2)
-            .move(corner.x - r, corner.y - r)
-            .translate(x, y)
-        })
+        const corners = this.corners()
+
+        this.draw = draw
+          .polygon(corners.map(({ x, y }) => `${x},${y}`))
+          .fill('blue')
+          .stroke({ width: 5, color: '#999' })
+          .translate(x, y)
       },
 
       highlight() {
@@ -63,29 +73,79 @@ export default {
         this.draw
           .fill({ opacity: 0, color: 'none' })
       }
-    })
+    });
     const Grid = Honeycomb.defineGrid(Hex)
 
     // render hexes
     const grid = Grid.spiral({ 
       radius: 2, 
-      center: Hex(2,2),
+      center: Hex(3,3),
+
       // render each hex, passing the draw instance
       onCreate(hex) {
         hex.render(draw)
       } 
     })
+    console.log(grid)
 
-    // Draw a settlement at each hex corner
-    grid.forEach(hex => {
-      console.log(hex)
-      console.log(hex.corners())
-      hex.renderSettlements(draw)
+    const oceanGrid = Grid.ring({ 
+      radius: 3, 
+      center: Hex(3,3),
+      // render each hex, passing the draw instance
+      onCreate(hex) {
+        hex.renderOcean(draw)
+      } 
+    })
+    console.log(oceanGrid)
+
+    // Create an array of settlement objects that contain the pixel coordinates of each settlement
+    const locateSettlements = () => {
+      const settlements = [];
+      const rowWidth = grid.radius + 1;
+      const maxRowWidth = grid.radius * 2 + 1;
+      let rowNumTop = 0;
+      let rowNumBottom = maxRowWidth;
+      for (let i = rowWidth; i <= maxRowWidth; i++) {
+        // Create settlements on the top half of the grid
+        const topHexes = grid.filter(hex => hex.y === rowNumTop + 1).sort((a,b) => a.x - b.x);
+        // Loops through each hex in the current row
+        topHexes.forEach( (hex, j) => {
+          let corners = hex.corners();
+          const { x, y } = hex.toPoint()
+          if (j === 0) {
+            settlements.push({x: 0, y: rowNumTop, point: {x: corners[4].x + x, y: corners[4].y + y} })
+          }
+          settlements.push({x: (j * 2) + 1, y: rowNumTop, point: {x: corners[5].x + x, y: corners[5].y + y} })
+          settlements.push({x: (j * 2) + 2, y: rowNumTop, point: {x: corners[0].x + x, y: corners[0].y + y} })
+        })
+        // Create settlements on the bottom half of the grid
+        const bottomHexes = grid.filter(hex => hex.y === rowNumBottom).sort((a,b) => a.x - b.x);
+        // Loops through each hex in the current row
+        bottomHexes.forEach( (hex, i) => {
+          let corners = hex.corners();
+          const { x, y } = hex.toPoint()
+          if (i === 0) {
+            settlements.push({x: 0, y: rowNumBottom, point: {x: corners[3].x + x, y: corners[3].y + y} })
+          }
+          settlements.push({x: (i * 2) + 1, y: rowNumBottom, point: {x: corners[1].x + x, y: corners[1].y + y} })
+          settlements.push({x: (i * 2) + 2, y: rowNumBottom, point: {x: corners[2].x + x, y: corners[2].y + y} })
+        })
+        rowNumTop++;
+        rowNumBottom--;
+      }
+      console.log(settlements);
+      return settlements;
+    }
+    const settlements = locateSettlements(grid)
+    settlements.forEach(settlement => {
+      renderSettlements(settlement);
     })
 
     // Add click listener to hexes
     document.addEventListener('click', ({ offsetX, offsetY }) => {
       const hexCoordinates = Grid.pointToHex([offsetX, offsetY])
+      console.log(hexCoordinates)
+      console.log(offsetX, offsetY)
       const hex = grid.get(hexCoordinates)
 
       if (hex) {
