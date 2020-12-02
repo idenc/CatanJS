4844ec11e40a34e1c0056d484e3696aa7f725a91
BrevinJohnston <brevin.ej@gmail.com>
2020-12-02 13:00:10 -0700

    initial place holders set up
    set up some variables i think we need for the games main logic

---
+++ server/gameLogic/game.js
@@ -0,0 +1,13 @@
+let tiles = {};
+let settlements = {};
+let roads = {};
+let longestRod = null;
+let largestArmy = null;
+let curentTurnIndex = 0;
+let players = {}; 
+let availableDevCards = {};
+
+
+module.exports = socket => {
+
+}
