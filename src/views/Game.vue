d9df7c65b73ef7878d6ab3d224ff937515a7e084
Dustin Doell <doelldd@gmail.com>
2020-12-04 10:06:18 -0700

    Added background pattern

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -62,11 +62,12 @@ export default {
 }
 
 #board-container {
-  background: #1b75bb;
   flex-grow: 5;
   flex-shrink: 1;
   height: 100vh;
   overflow: hidden;
+  background-color: #1b75bb;
+  background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%237abbdf' fill-opacity='0.61'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 }
 
 #sidebar-container {
